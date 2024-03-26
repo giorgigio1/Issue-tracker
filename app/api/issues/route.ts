215025d62ef09039a441e00c404268c5f4406f2a
giorgigio1 <giorgigiorgdze93@gmail.com>
2024-03-26 15:11:30 +0400

    Build an api for creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/client";
+
+const createIssueSchema = z.object({
+  title: z.string().min(3).max(255),
+  description: z.string().min(3),
+});
+
+export async function GET(request: NextRequest) {
+  const users = await prisma.issue.findMany();
+  return NextResponse.json(users, { status: 200 });
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+  return NextResponse.json(newIssue, { status: 201 });
+}
