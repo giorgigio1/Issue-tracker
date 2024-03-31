bd9f8346b816827ceabeeaf9681a8f5abab86651
giorgigio1 <giorgigiorgdze93@gmail.com>
2024-03-31 23:26:59 +0400

    Disable SSR for loading md editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,23 @@
 "use client";
 
 import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
 import { useState } from "react";
 import axios from "axios";
 import { Button, Callout, TextField } from "@radix-ui/themes";
 import { useForm, Controller } from "react-hook-form";
-import { SimpleMdeReact } from "react-simplemde-editor";
 import { createIssueSchema } from "@/app/validationSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
 import { useRouter } from "next/navigation";
+
+// import { SimpleMdeReact } from "react-simplemde-editor";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 type IssueProps = z.infer<typeof createIssueSchema>;
 
@@ -57,7 +63,7 @@ const NewIssuesPage = () => {
         <Controller
           name="description"
           control={control}
-          render={({ field }) => <SimpleMdeReact {...field} />}
+          render={({ field }) => <SimpleMDE {...field} />}
         />
         <Button disabled={isSubmitting}>
           Create Issue{isSubmitting && <Spinner />}
