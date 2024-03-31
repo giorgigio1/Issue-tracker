77951530dea375af9b55d0a5c0ee1ce035909e3c
giorgigio1 <giorgigiorgdze93@gmail.com>
2024-03-31 23:49:43 +0400

    Refactor: Organize import statements

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,19 +1,17 @@
 "use client";
 
-import "easymde/dist/easymde.min.css";
-import dynamic from "next/dynamic";
-import { useState } from "react";
-import axios from "axios";
-import { Button, Callout, TextField } from "@radix-ui/themes";
-import { useForm, Controller } from "react-hook-form";
+import { ErrorMessage } from "@/app/components/index";
+import Spinner from "@/app/components/Spinner";
 import { createIssueSchema } from "@/app/validationSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import ErrorMessage from "@/app/components/ErrorMessage";
-import Spinner from "@/app/components/Spinner";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import axios from "axios";
+import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
-
-// import { SimpleMdeReact } from "react-simplemde-editor";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
