ab85326e929f32c454e8d3ad1b97b62f74499249
giorgigio1 <giorgigiorgdze93@gmail.com>
2024-04-09 21:59:00 +0400

    Secure the application

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,6 +1,8 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { issueSchema } from "../../validationSchema";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/auth/authOptions";
 
 export async function GET(request: NextRequest) {
   const users = await prisma.issue.findMany();
@@ -8,6 +10,9 @@ export async function GET(request: NextRequest) {
 }
 
 export async function POST(request: NextRequest) {
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
+
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
   if (!validation.success)
