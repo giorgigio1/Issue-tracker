4a3269bdc28ca02460ebe147782a133ec98c3853
giorgigio1 <giorgigiorgdze93@gmail.com>
2024-04-19 21:24:14 +0400

    Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -22,11 +22,16 @@ const IssuesPage = async ({
     ? searchParams.status
     : undefined;
 
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "desc" }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: { status },
+    orderBy,
   });
-
-  const query = {};
 
   return (
     <div>
